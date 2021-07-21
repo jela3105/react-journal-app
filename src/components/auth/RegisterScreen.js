51dd4a0b74b0b95c4dc468b1c1c9db963d8bb6b1
jela3105 <jela3105@gmail.com>
2021-07-20 20:52:36 -0500

    Create form for register screen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+        <input
+          type="text"
+          className="auth__input"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          className="auth__input"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          className="auth__input"
+          placeholder="Password"
+          name="password"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          className="auth__input"
+          placeholder="Confirm password"
+          name="confirm-password"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn-primary btn-block mb-5">
+          Register
+        </button>
+        {`Already registerd? `}
+        <Link className="link " to="/auth/login">
+          Login here
+        </Link>
+      </form>
+    </>
   );
 };
