48f5b3c85fa43eecbb0d64996084f91096a63f6c
jela3105 <jela3105@gmail.com>
2021-07-23 22:33:28 -0500

    Add styles to NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -4,6 +4,23 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+        />
+        <textarea
+          className="notes__textarea"
+          placeholder="what happent today"
+        ></textarea>
+        <div className="notes__image">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+            alt="someting awesome"
+          />
+        </div>
+      </div>
     </div>
   );
 };
