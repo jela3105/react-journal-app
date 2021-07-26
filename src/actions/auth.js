7689c54f3b371714689b0f51388b6b23653701f4
jela3105 <jela3105@gmail.com>
2021-07-26 10:56:04 -0500

    Implement dispatch for asyc action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,13 @@
 import { types } from "../types/types";
 
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(email, password));
+    }, 3500);
+  };
+};
+
 export const login = (uid, displayName) => ({
   type: types.login,
   payload: {
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { useForm } from "../../hooks/useForm";
-import { login } from "../../actions/auth";
+import { startLoginEmailPassword } from "../../actions/auth";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -17,7 +17,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch(login(12345, "Jela"));
+    dispatch(startLoginEmailPassword(email, password));
   };
 
   return (
