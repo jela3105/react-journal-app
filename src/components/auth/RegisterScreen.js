d116a0dd90d6b7d80ecd0d663207d5f1d7d65b4a
jela3105 <jela3105@gmail.com>
2021-07-26 14:02:56 -0500

    Implement useForm hook for registerScreen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 
 export const RegisterScreen = () => {
+  const [registerValues, handleInputChange] = useForm({
+    name: "jela",
+    email: "jela@.com",
+    password: "123456",
+    confirmPassword: "123456",
+  });
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(registerValues);
+  };
+
   return (
     <>
       <h3 className="auth__title">Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
         <input
           type="text"
           className="auth__input"
           placeholder="Name"
           name="name"
           autoComplete="off"
+          value={registerValues.name}
+          onChange={handleInputChange}
         />
         <input
           type="text"
@@ -19,6 +34,8 @@ export const RegisterScreen = () => {
           placeholder="Email"
           name="email"
           autoComplete="off"
+          value={registerValues.email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
@@ -26,13 +43,17 @@ export const RegisterScreen = () => {
           placeholder="Password"
           name="password"
           autoComplete="off"
+          value={registerValues.password}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           className="auth__input"
           placeholder="Confirm password"
-          name="confirm-password"
+          name="confirmPassword"
           autoComplete="off"
+          value={registerValues.confirmPassword}
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btn-primary btn-block mb-5">
           Register
