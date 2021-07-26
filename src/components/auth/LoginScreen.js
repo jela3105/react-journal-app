5c256329aa343654de350dab9fb4ee9113e2cd6a
jela3105 <jela3105@gmail.com>
2021-07-26 13:50:07 -0500

    Implement auth with google sing-in

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { useForm } from "../../hooks/useForm";
-import { startLoginEmailPassword } from "../../actions/auth";
+import { startGoogleLogin, startLoginEmailPassword } from "../../actions/auth";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -18,6 +18,10 @@ export const LoginScreen = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(startLoginEmailPassword(email, password));
+  };
+
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
   };
 
   return (
@@ -47,7 +51,7 @@ export const LoginScreen = () => {
         </button>
         <div className="auth__social-media">
           <p>Log in with social media</p>
-          <div className="google-btn">
+          <div className="google-btn" onClick={handleGoogleLogin}>
             <div className="google-icon-wrapper">
               <img
                 className="google-icon"
