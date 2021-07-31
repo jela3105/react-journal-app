a877223df9a87522a5c6c434230d5e066b38afa5
jela3105 <jela3105@gmail.com>
2021-07-30 22:29:06 -0500

    Disable login button on loading with redux

---
+++ src/actions/ui.js
@@ -0,0 +1,5 @@
+import { types } from "../types/types";
+
+export const startLoading = () => ({ type: types.uiStartLoading });
+
+export const finishLoading = () => ({ type: types.uiFinishLoading });
---
+++ src/reducers/uiReducer.js
@@ -0,0 +1,13 @@
+import { types } from "../types/types";
+
+export const uiReducer = (state = {}, action) => {
+  switch (action.type) {
+    case types.uiStartLoading:
+      return { ...state, loading: true };
+
+    case types.uiFinishLoading:
+      return { ...state, loading: false };
+
+    default:
+  }
+};
