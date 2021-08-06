876e5c74533a1835176be9ad39c8069ca1d25610
jela3105 <jela3105@gmail.com>
2021-08-06 15:14:48 -0500

    Active created note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebase-config";
+import { types } from "../types/types";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -6,6 +7,11 @@ export const startNewNote = () => {
     const newNote = { title: "", body: "", date: new Date().getTime() };
 
     const docRef = await db.collection(`${uid}/journal/notes`).add(newNote);
-    console.log(docRef);
+    dispatch(activeNote(docRef.id, newNote));
   };
 };
+
+export const activeNote = (id, note) => ({
+  type: types.notesActive,
+  payload: { id, ...note },
+});
