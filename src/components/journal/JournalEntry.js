c673fc6cc88ed216ea8efae7ad48362226d8e38b
jela3105 <jela3105@gmail.com>
2021-08-07 16:22:10 -0500

    Show notes in the Sidebar

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,23 +1,20 @@
 import React from "react";
 
-export const JournalEntry = () => {
+export const JournalEntry = (props) => {
   return (
     <div className="journal__entry pointer">
-      <div
-        className="journal__entry-picture"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage:
-            "url(https://en.es-static.us/upl/2018/12/comet-wirtanen-Jack-Fusco-dec-2018-Anza-Borrego-desert-CA-e1544613895713.jpg)",
-        }}
-      ></div>
+      {props.url && (
+        <div
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage: `url(${props.url})`,
+          }}
+        ></div>
+      )}
       <div className="journal__entry-body">
-        <p className="journal__entry-title">
-          Keep ur bliss there is nothing wrong with this
-        </p>
-        <p className="journal__entry-content">
-          We're moving sideways Welcome to Mulberry Street{" "}
-        </p>
+        <p className="journal__entry-title">{props.title}</p>
+        <p className="journal__entry-content">{props.body}</p>
       </div>
       <div className="journal__entry-date-box">
         <span>Monday</span>
