29f0f63b040754157219e2427f1168f892d6b5c1
jela3105 <jela3105@gmail.com>
2021-08-08 19:14:37 -0500

    Change state on click note

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { NotesAppBar } from "./NotesAppBar";
+import { useForm } from "../../hooks/useForm";
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange] = useForm(note);
+  const { body, title } = formValues;
+
+  useEffect(() => {}, []);
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -9,17 +17,24 @@ export const NoteScreen = () => {
           type="text"
           placeholder="Some awesome title"
           className="notes__title-input"
+          autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
         />
         <textarea
           className="notes__textarea"
           placeholder="what happent today"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
-        <div className="notes__image">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-            alt="someting awesome"
-          />
-        </div>
+        {note.url && (
+          <div className="notes__image">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+              alt="someting awesome"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
