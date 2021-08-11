1cd5c959db1553898202f2af560c3c25ac80bd62
jela3105 <jela3105@gmail.com>
2021-08-10 20:23:29 -0500

    Select file on click picture button

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { startSaveNote } from "../../actions/notes";
 
+//https://api.cloudinary.com/v1_1/jela3105
 export const NotesAppBar = () => {
   const dispatch = useDispatch();
   const { active } = useSelector((state) => state.notes);
@@ -9,11 +10,29 @@ export const NotesAppBar = () => {
   const handleSave = () => {
     dispatch(startSaveNote(active));
   };
+
+  const handlePictureClick = () => {
+    document.querySelector("#fileSelector").click();
+  };
+
+  const handleFileChange = (e) => {
+    console.log(e.target.files);
+  };
+
   return (
     <div className="notes__appbar">
       <span>august 30 2020</span>
+      <input
+        id="fileSelector"
+        type="file"
+        name="file"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
       <div>
-        <button className="btn">Picture</button>
+        <button onClick={handlePictureClick} className="btn">
+          Picture
+        </button>
         <button onClick={handleSave} className="btn">
           Save
         </button>
